d8d76a77e79d8ed798cc64fc4a46962a116c1670
Matthew <pugmatt@gmail.com>
2019-03-18 19:57:54 -0400

    Multiple dish images

--- source/frontend/src/app/dish/dish.component.ts
+++ source/frontend/src/app/dish/dish.component.ts
@@ -4,7 +4,8 @@ import { ActivatedRoute, Router } from '@angular/router';
 
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
 
-import {DishService} from "./dish.service";
+import { DishService } from "./dish.service";
+import { UserService } from "../user.service";
 
 import { CreateDishComponent } from './create-dish/create-dish.component';
 
@@ -20,11 +21,14 @@ export class DishComponent implements OnInit {
   public dish: Dish;
   
   constructor(private dishService: DishService,
+  private userService: UserService,
   private route: ActivatedRoute,
   private router: Router,
   public dialog: MatDialog) { }
 
   public error: String;
+
+  public currentImage: number = 1;
 
   ngOnInit() {
     this.dishService.getDish(this.route.snapshot.params.id)
@@ -34,11 +38,22 @@ export class DishComponent implements OnInit {
     });
   }
 
+  // Change image based on selection
+  changeImage(index: number) {
+    this.currentImage = index;
+  }
+
+  // Return API url based on image index
+  getUrl(index: number) {
+    return 'url(/api/images/dish/' + this.dish.id + '/' + (index+1) + ')';
+  }
+
   add() {
     let dialogRef = this.dialog.open(CreateDishComponent, {
     });
   }
 
+  // Create range for gallery looping through {{disk.images}} images
   createRange(number){
     var items: number[] = [];
     for(var i = 1; i <= number; i++){
